86dba11eb7d7b48fc3d27b3b60a6aa6d5a36be3c
phewphewer <joshvvidal23@gmail.com>
2025-02-15 12:37:43 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Sticky Navbar on Scroll
+let lastScrollTop = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-80px"; // Hide navbar when scrolling down
+    } else {
+        navbar.style.top = "0"; // Show navbar when scrolling up
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
